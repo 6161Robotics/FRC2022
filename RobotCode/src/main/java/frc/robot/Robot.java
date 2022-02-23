bf00b19adad62f409617980970282c15efe27f1b
Stephen Goss <goss.stephenm@gmail.com>
2022-02-22 21:36:46 -0500

    Cleaned up comments

--- RobotCode/src/main/java/frc/robot/Robot.java
+++ RobotCode/src/main/java/frc/robot/Robot.java
@@ -20,48 +20,38 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
+  // The drive base object
   private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-  private XboxController _controller;
+  // The Drivers Controller object
+  private XboxController m_driverController;
   
+  // The Left Side Motor Controllers
+  CANSparkMax m_frontLeftSpark = new CANSparkMax(22, MotorType.kBrushless);
+	CANSparkMax m_rearLeftSpark = new CANSparkMax(23, MotorType.kBrushless);
+  // The Right Side Motor Controllers
+  CANSparkMax m_frontRightSpark = new CANSparkMax(21, MotorType.kBrushless);
+  CANSparkMax m_rearRightSpark = new CANSparkMax(24, MotorType.kBrushless);
+  // Group the motor controllers per side
+  MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeftSpark, m_rearLeftSpark);
+  MotorControllerGroup m_right = new MotorControllerGroup(m_frontRightSpark, m_rearRightSpark);
 
-  //private final MotorController m_leftMotor = new PWMSparkMax(0);
-  //private final MotorController m_rightMotor = new PWMSparkMax(1);
-
-  /* Master Talons for arcade drive */
-  // Swapped CAN IDs to get it to drive the right direction
-  CANSparkMax _frontLeftSpark = new CANSparkMax(22, MotorType.kBrushless);
-  CANSparkMax _frontRightSpark = new CANSparkMax(21, MotorType.kBrushless);
-
-	//WPI_TalonSRX _frontLeftMotor = new WPI_TalonSRX(1);
-	//WPI_TalonSRX _frontRightMotor = new WPI_TalonSRX(2);
-
-	/* Follower Talons + Victors for six motor drives */
-	CANSparkMax _rearLeftSpark = new CANSparkMax(23, MotorType.kBrushless);
-  CANSparkMax _rearRightSpark = new CANSparkMax(24, MotorType.kBrushless);
-  MotorControllerGroup m_left = new MotorControllerGroup(_frontLeftSpark, _rearLeftSpark);
-  MotorControllerGroup m_right = new MotorControllerGroup(_frontRightSpark, _rearRightSpark);
-  //WPI_TalonSRX _leftFollower1 = new WPI_TalonSRX(5);
-	//WPI_VictorSPX _rightFollower1 = new WPI_VictorSPX(7);
-	//WPI_TalonSRX _leftFollower2 = new WPI_TalonSRX(4);
-	//WPI_VictorSPX _rightFollower2 = new WPI_VictorSPX(17);
-  
+  /**
+   * This code is called when the robot starts to execute
+   */
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_right.setInverted(true);
-
+    // Populate the drive base object
     m_myRobot = new DifferentialDrive(m_left, m_right);
-    //m_leftStick = new Joystick(0);
-    //m_rightStick = new Joystick(1);
-    _controller = new XboxController(0);
+    // Populate the driver controller object
+    m_driverController = new XboxController(0);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(_controller.getLeftY(), _controller.getRightY());
+    m_myRobot.tankDrive(m_driverController.getLeftY(), m_driverController.getRightY());
   }
 }
